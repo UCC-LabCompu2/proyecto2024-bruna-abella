a0addb2048651372dedccc86f4b866d8ec7dc932
Max20050 <maximoabella12@gmail.com>
2024-06-17 12:16:42 -0300

    Fix #22
    Arreglados issues generales del javascript. Falta implementar las sugerencias.

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-var canvas = document.getElementById("canvas"),
+var canvas = document.getElementById("canvas"), // Variable Global del canvas. Es necesaria para la funcion dibujar.
     ctx = canvas.getContext("2d");
 
-// Calculos previos para todas las funciones a graficar. <<<--- Para el canvas.
 
-const imagen = document.getElementById("bg-img");
-ctx.drawImage(imagen, 24, -68);
-const scale = 100;
-var pixelStep = 1 / scale;
-w = canvas.width; // El ancho del canvas
-h = canvas.height; // El largo del canvas
-step = w / 40 * scale;
-var pixel = w / 40; // La cantidad de pixeles en el canvas que equivalen a un valor de x.
-const yscale = h / 20; // La cantidad de pixeles en el canvas que equivalen a un valor de y.
-ctx.translate(canvas.width / 2, canvas.height / 2); // Movemos nuestro (0,0) al centro del canvas.
-
+window.onload = function() {
+    const imagen = new Image();
+    imagen.src = "images/grid.png";
+    ctx.drawImage(imagen, 24, -68);
+    ctx.translate(canvas.width / 2, canvas.height / 2); 
+};
 
 /**
  * FUncion Que Pone una alerta general al usuario.
  * @method AlertaERRORCampo
  */
 
-function AlertaERRORCampo() { // Si no se ingresa una funcion valida se le alertara al usuario.
+let AlertaERRORCampo = () => { // Si no se ingresa una funcion valida se le alertara al usuario.
     alert("ERROR: La funcion ingresada no se puede graficar!");
 }
 
@@ -31,7 +25,7 @@ function AlertaERRORCampo() { // Si no se ingresa una funcion valida se le alert
  * @returns true o false
  */
 
-function checkValido(expresion) {
+let checkValido = (expresion) => {
     let distintos = 0;
     for (let i = -20; i < 20; i++) {
 
@@ -60,10 +54,19 @@ function checkValido(expresion) {
  * @param {string} color - Pasa el color de la funcion a graficar.
  * @param {int} ancho - Pasa el ancho de la funcion a graficar.
  */
-function dibujar(funcion, color, ancho) {
+let dibujar = (funcion, color, ancho) => {
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.lineWidth = ancho;
+
+    const scale = 100;
+    var pixelStep = 1 / scale;
+    w = canvas.width; // El ancho del canvas
+    h = canvas.height; // El largo del canvas
+    step = w / 40 * scale;
+    var pixel = w / 40; // La cantidad de pixeles en el canvas que equivalen a un valor de x.
+    const yscale = h / 20; // La cantidad de pixeles en el canvas que equivalen a un valor de y.
+
     for (let x = -20; x < 20; x += pixelStep) {
         setTimeout(function () { // Usamos timeout para simular una animacion al momento de hacer los calculos y graficar.
             let y = (funcion(x) * yscale);
@@ -80,11 +83,12 @@ function dibujar(funcion, color, ancho) {
     }
 }
 
-let graficoForm = document.getElementById("form-graph"); // Objeto de formulario.
 
-graficoForm.addEventListener("submit", (form) => {
+
+
+let generarGrafico = (form) => {
     form.preventDefault();
-    let funciontext = (document.getElementById("funcion").value);
+    const funciontext = (document.getElementById("funcion").value);
     if (funciontext == "" || funciontext == " ") {
         AlertaERRORCampo();
     }
@@ -97,10 +101,10 @@ graficoForm.addEventListener("submit", (form) => {
         AlertaERRORCampo();
         document.getElementById("funcion").value = "";
     }
-});
+}
 
 
-function insert_b(value) {
+let insert_b = (value) => {
     let fun = document.getElementById("funcion");
     fun.value = fun.value + value;
 }
